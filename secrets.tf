fc74ba6f0409b6885f520c986530848ff221a039
Ramesh <csramesh0@gmail.com>
2025-04-25 10:25:52 +0530

    2

--- secrets.tf
+++ secrets.tf
@@ -1,16 +1,19 @@
 
+locals {
+  drupal_secret_values = {
+    DB_HOST     = aws_db_instance.drupal.address
+    DB_NAME     = var.db_name
+    DB_USER     = var.db_user
+    DB_PASSWORD = var.db_password
+  }
+}
+
 resource "aws_secretsmanager_secret" "drupal_db_config" {
-  name        = "prod/drupal/db-config-v8"
+  name        = "prod/drupal/db-config-v10"
   description = "Drupal database credentials for ECS"
 }
 
 resource "aws_secretsmanager_secret_version" "drupal_db_config_version" {
   secret_id     = aws_secretsmanager_secret.drupal_db_config.id
-
-  secret_string = jsonencode({
-    DB_HOST     = "drupal-db.cluster-xxxxx.ap-southeast-1.rds.amazonaws.com",
-    DB_NAME     = var.db_name,
-    DB_USER     = var.db_user,
-    DB_PASSWORD = var.db_password
-  })
+  secret_string = jsonencode(local.drupal_secret_values)
 }
